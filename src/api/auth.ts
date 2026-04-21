4c6237ea3e13d9fe443cf3e1f3a584227c4076c7
Yohanes Bonar Hariningtyas <yohanesbonar@gmail.com>
2026-04-21 14:32:51 +0700

    remove unused log

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -33,7 +33,7 @@ export type RegisterOtpVerifyResponse = {
       currentStep: string;
       expiredAt: string;
       sessionId: string;
-    }
+    };
   };
 };
 
@@ -46,9 +46,9 @@ export type RegisterPinSetupResponse = {
       accessToken: string;
       expiredIn: string;
       refreshToken: string;
-    }
+    };
   };
-}
+};
 
 // LOGIN
 export interface LoginOtpRequestPayload {
@@ -83,45 +83,53 @@ export type LoginOtpVerifyResponse = {
       currentStep: string;
       expiredAt: string;
       sessionId: string;
-    }
+    };
   };
 };
 
 export type LoginResponse = {
-  status: string; 
+  status: string;
   message: string;
-  data: {
-
-  }
-}
-
-
+  data: {};
+};
 
 export const authApi = {
   registerRequestOtp: async (payload: RegisterOtpRequestPayload): Promise<RegisterOtpResponse> => {
-    const { data } = await apiClient.post<RegisterOtpResponse>('/v1/onboarding/otp/request', payload);
+    const { data } = await apiClient.post<RegisterOtpResponse>(
+      '/v1/onboarding/otp/request',
+      payload,
+    );
     return data;
   },
-  registerVerifyOtp: async (payload: RegisterOtpVerifyPayload): Promise<RegisterOtpVerifyResponse> => {
-    const { data } = await apiClient.post<RegisterOtpVerifyResponse>('/v1/onboarding/otp/verify', payload);
+  registerVerifyOtp: async (
+    payload: RegisterOtpVerifyPayload,
+  ): Promise<RegisterOtpVerifyResponse> => {
+    const { data } = await apiClient.post<RegisterOtpVerifyResponse>(
+      '/v1/onboarding/otp/verify',
+      payload,
+    );
     return data;
   },
   registerPinSetup: async (payload: RegisterPinSetupPayload): Promise<RegisterPinSetupResponse> => {
-    const { data } = await apiClient.post<RegisterPinSetupResponse>('/v1/onboarding/pin-setup', payload);
-    return data; 
-  }, 
+    const { data } = await apiClient.post<RegisterPinSetupResponse>(
+      '/v1/onboarding/pin-setup',
+      payload,
+    );
+    return data;
+  },
   loginRequestOtp: async (payload: LoginOtpRequestPayload): Promise<LoginOtpResponse> => {
     const { data } = await apiClient.post<LoginOtpResponse>('v1/auth/login/otp/request', payload);
     return data;
   },
   loginVerifyOtp: async (payload: LoginOtpVerifyPayload): Promise<LoginOtpVerifyResponse> => {
-    const { data } = await apiClient.post<LoginOtpVerifyResponse>('v1/auth/login/otp/verify', payload);
+    const { data } = await apiClient.post<LoginOtpVerifyResponse>(
+      'v1/auth/login/otp/verify',
+      payload,
+    );
     return data;
-  }, 
+  },
   login: async (payload: LoginPayload): Promise<LoginResponse> => {
     const { data } = await apiClient.post<LoginResponse>('v1/auth/login', payload);
     return data;
-  }
-  // Contoh penambahan fungsi lain nanti:
-  // login: (credentials) => apiClient.post('/login', credentials),
+  },
 };
